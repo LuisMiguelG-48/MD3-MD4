c575e2187ddc8b638a68c13b61a1139a6b988dc5
JonathanBeltran <101604385+JonathanBeltran@users.noreply.github.com>
2022-11-02 17:57:58 -0500

    Update 4.3.1.15 cisco python.py

--- 4.3.1.15 cisco python.py
+++ 4.3.1.15 cisco python.py
@@ -1,17 +1,17 @@
-from os import strerror
+from os  import  strerror
 
-counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
-nm_fl= input("¿Cual es el nombre del archivo?: ")
-try:
-    file = open(nm_fl, "rt")
-    for line in file:
-        for ch in line:
-            if ch.isalpha():
-                counters[ch.lower()] += 1
-    file.close()
-    for ch in counters.keys():
-        c = counters[ch]
-        if c > 0:
-            print(ch, '->', c)
-except IOError:
-    print("Error: ", strerror(IOError.errno))
+contadores  = { chr ( ch ): 0  for  ch  in  range ( ord ( 'a' ), ord ( 'z' ) +  1 )}
+f_name  =  input ( "Ingresa el nombre del archivo: " )
+try :
+    archivo  =  open ( f_name , "rt" )
+    for  line  in  archivo :
+        for  char  in  line :
+            if  char.isalpha ():
+                contadores [ char.lower ()] +=  1
+    archivo.close ()
+    for  char  in  contadores.keys ():
+        c  =  contadores [ char ]
+        if  c  >  0 :
+            print ( char , '->' , c )
+except  IOError as e:
+    print ( "Error: " , strerror ( e.errno ))
